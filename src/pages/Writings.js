c88383511a92cbe61bfb8d7220ae79812a523b42
Smashthehedgehog <mikeani482@gmail.com>
2024-05-27 21:12:18 -0400

    blog

--- src/pages/Writings.js
+++ src/pages/Writings.js
@@ -13,6 +13,15 @@ const Writings = () => {
                     this website.
                 </p>
             </div>
+            <div className='d-flex flex-column blog-container gap-4'>
+              <div className='blog-card position-relative'>
+                  <p className='text-light headline-5-medium'>MAY 27, 2024</p>
+                  <p className='headline-1-large sonic-beige-text'>The Rough Draft of the Website</p>
+                  <p className='headline-5-large text-light'>My thoughts on why I started this website in the first place
+                  and my progress on it so far</p>
+              </div>
+              <div className='blog-card position-relative'></div>
+            </div>
         </div>
     </div>
   )
