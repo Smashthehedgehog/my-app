1535a0476fe872b440c05c2ad5f0783f0a87052a
Smashthehedgehog <mikeani482@gmail.com>
2024-06-05 22:09:04 -0400

    ee

---
+++ src/pages/Writing_TheRoughDraftOfTheWebsite.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Writing_TheRoughDraftOfTheWebsite = () => {
+  return (
+    <div className="App-content-stuff d-flex flex-column">
+        <div className='blog-container'>
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default Writing_TheRoughDraftOfTheWebsite
