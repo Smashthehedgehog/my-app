1d89b7b6ad39dffce9a86c808c4f256b8366e220
Smashthehedgehog <mikeani482@gmail.com>
2024-06-10 21:23:59 -0400

    did some work on writing

--- src/pages/Writing_TheRoughDraftOfTheWebsite.js
+++ src/pages/Writing_TheRoughDraftOfTheWebsite.js
@@ -16,11 +16,25 @@ const Writing_TheRoughDraftOfTheWebsite = () => {
             <p className='newspaper-body'>When I graduated, I was trying to do anything to improve my chances of
             getting a job, and one thing I noticed I didn't have was a proper profile. The realization hit me when 
             I went on my co-intern from Amdocs Tom's LinkedIn, as he graduated as well. The first thing I noticed was 
-            that he has a link to <Link to='https://www.tomkastoryano.com/' target='_blank' className='no-decoration'>his own website,</Link> 
+            that he has a link to <Link to='https://www.tomkastoryano.com/' target='_blank' className='no-decoration'>his own website</Link>, 
             which he created from React.</p>
             <p className='newspaper-body'>Inspired by his creation, I wanted to look at multiple website portfolios that 
-            were created from scratch, avoiding the use of a template so I can hone my Frontend
-            development skills naturally.</p>
+            were created from scratch, avoiding the use of a template so I can hone my frontend
+            development skills naturally. I ended up finding <Link to='https://www.tomkastoryano.com/' target='_blank' className='no-decoration'>this portfolio </Link>
+            by Robb Owen, an astounding showcase of web development whih several features:</p>
+            <ul className='newspaper-body'>
+                <li> Consideration of Accessibility. </li>
+                <li> A multitude of pages.</li>
+                <li> A responsive dropdown submenu.</li>
+                <li> A layered SVG dependent on the mouse location.</li>
+            </ul>
+            <p className='newspaper-body'>From that, I decided to jump right in after looking up how to start a React app and go from there. I 
+            made a Figma account but once I envisioned how I wanted my site to look like, I just started coding without putting anything into Figma. 
+            I do think that I should have started with the Figma first but I couldn't help myself. It took a lot of Googling as well as I got stuck at
+            a bunch of points, such as how to add animations, routers, icons, and more. Of course, that was nothing that 'npm install' couldn't fix.
+            However, what gave me the biggest issue was the sidebar. I had trouble figuring out how to construct it. Even in it's current state, I still
+            have issues with Accessiblity, especially when on focus:</p>
+            <div></div>
             
             
 
